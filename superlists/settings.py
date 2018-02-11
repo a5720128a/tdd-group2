608c2f3309c061cbe63081cd380de30f7f375fd0
suppakan <suppakan@ubuntu.ubuntu-domain>
2018-02-11 21:47:48 +0700

    Ensuring We Have a Regression Test

--- superlists/settings.py
+++ superlists/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
